33d4b1f536036baa764517b1b769a6c535a81901
SagarKadam2906 <sagar@tradecred.com>
2022-04-02 16:46:22 +0530

    txt detection proj added

---
+++ text_detection/text_extraction.py
@@ -0,0 +1,11 @@
+import cv2 as cv
+import glob
+
+# text detection and extraction from image
+
+directory = '/Users/sagar/Downloads'
+image_formats = ['png', 'jpg', 'jpeg', 'gif']
+
+print(directory)
+
+
